954ccc117106e710258e9477e55d58a0e7f4646b
liwei <fanxiaobin@liweideMacBook-Air.local>
2017-03-23 14:19:18 +0800

    Initial Commit

---
+++ MarqueeView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MarqueeView
+//
+//  Created by fanxiaobin on 2017/3/23.
+//  Copyright © 2017年 fanxiaobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
